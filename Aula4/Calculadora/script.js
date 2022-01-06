f3db665e83b998fbef468dce419ef974a51dda4f
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-01-06 15:03:17 -0300

    algumas revisoes no codigo

--- Aula4/Calculadora/script.js
+++ Aula4/Calculadora/script.js
@@ -1,34 +1,34 @@
 function calcular(){
-    var operacao = document.getElementById("operacao").value
-    var num1 = document.getElementById("num1").value
-    var num2 = document.getElementById("num2").value
+    var operacao = document.getElementById("operacao").value //pega o valor da operacao
+    var num1 = document.getElementById("num1").value //pega o 1 valor  
+    var num2 = document.getElementById("num2").value //pega o 2 valor
   
-    if(num1 == '' || num1 == null){
-      alert('Favor digitar um número válido')
-      document.getElementById('num1').focus()
+    if(num1 == '' || num1 == null){ //verifica se o num1 esta vazio
+      alert('Favor digitar um número válido') //alerta caso não tenha número
+      document.getElementById('num1').focus() 
       return false
     }
-    if(num2 == '' || num2 == null){
-      alert('Favor digitar um número válido')
+    if(num2 == '' || num2 == null){ //verifica se o num2 esta vazio
+      alert('Favor digitar um número válido') //alerta caso não tenha número
       document.getElementById('num2').focus()
       return false
     }
   
     var resultado = null
   
-    num1 = Number(num1)
-    num2 = Number(num2)
+    num1 = Number(num1) //converte a entrada para inteiro
+    num2 = Number(num2) //converte a entrada para inteiro
   
-    if(operacao == 1){
+    if(operacao == 1){ //soma
       resultado = num1+num2
-    }else if(operacao == 2){
+    }else if(operacao == 2){ //subtração
       resultado = num1-num2
-    }else if(operacao == 3){
+    }else if(operacao == 3){ //multiplicação
       resultado = num1*num2
-    }else if(operacao == 4){
+    }else if(operacao == 4){ //divisao
       resultado = num1/num2
     }else{
-      alert('Selecione uma operação')
+      alert('Selecione uma operação') //alerta caso não tenha uma opcao de operacao
       return false
     }
   
